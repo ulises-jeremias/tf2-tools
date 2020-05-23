855ef7660bdeca8d4a340b81a28d695c4e007892
ulises-jeremias <ulisescf.24@gmail.com>
2020-05-23 16:05:54 -0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+from setuptools import setup
+
+setup(name='tf_tools',
+      version='0.0.1',
+      license='MIT',
+      packages=['tf_tools'],
+      install_requires=['Pillow'])
